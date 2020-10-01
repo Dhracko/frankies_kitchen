5617689a0d8bf85da98b71136a9c6accf0151c9e
Frankie <dracko@hotmail.co.uk>
2020-10-01 19:47:40 +0000

    base.html created

--- app.py
+++ app.py
@@ -1,2 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
+
+MONGODB_URI = os.environ.get('MONGO_URI')
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'frankie_recipes'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html',
+                            recipes=mongo.db.recipes.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
