b8867a2291cd29c811f3433cb7acdccb3ad86d69
Frankie <dracko@hotmail.co.uk>
2020-10-24 22:02:02 +0000

    Created editrecipe.html and /update python code

--- app.py
+++ app.py
@@ -25,15 +25,7 @@ def get_recipes():
                            recipes=mongo.db.recipes.find())
 
 
-# Recipe Page
-
-@app.route("/recipe/<recipe_id>/<recipe_name>")
-def gt_recipe(recipe_id, recipe_name):
-    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template("recipe.html", recipe=the_recipe)
-
-
-# Add recipe page
+# Create recipe page
 
 @app.route('/add_recipe')
 def add_recipe():
@@ -41,26 +33,78 @@ def add_recipe():
                            categories=mongo.db.categories.find())
 
 
-@app.route('/insert_recipe', methods=['GET', 'POST'])
+@app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    if request.method == 'POST':
-        rec = request.form
+    rec = request.form
 
-        name = rec["recipe_name"]
-        description = rec["recipe_description"]
-        prep_time = rec["preparation_time"]
-        cook_time = rec["cooking_time"]
-        skill = rec["skill"]
-        portions = rec["portions"]
+    name = rec["recipe_name"]
+    description = rec["recipe_description"]
+    prep_time = rec["preparation_time"]
+    cook_time = rec["cooking_time"]
+    skill = rec["skill"]
+    portions = rec["portions"]
 
-        """
-        Below is the Ingredients and Steps which were
-        predeterment in the function before
-        """
-        ingredient_list = recipe_ingredients(rec)
-        steps_list = recipe_steps(rec)
+    """
+    Below is the Ingredients and Steps which were
+    predeterment in the function before
+    """
+    ingredient_list = recipe_ingredients(rec)
+    steps_list = recipe_steps(rec)
 
-        new_recipe = {
+    new_recipe = {
+        "recipe_name": name,
+        "recipe_description": description,
+        "recipe_prep_time": int(prep_time),
+        "recipe_cooking_time": int(cook_time),
+        "recipe_skill": skill,
+        "recipe_portions": int(portions),
+        "recipe_steps": steps_list,
+        "recipe_ingredients": ingredient_list,
+    }
+
+    recipe = mongo.db.recipes
+    recipe.insert_one(new_recipe)
+    return redirect(url_for('get_recipes'))
+
+
+# Read Recipe Page
+
+@app.route("/recipe/<recipe_id>/<recipe_name>")
+def gt_recipe(recipe_id, recipe_name):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template("recipe.html", recipe=the_recipe)
+
+
+# Update / Edit Recipe
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('editrecipe.html', recipe=the_recipe)
+
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipe = mongo.db.recipes
+    rec = request.form
+
+    name = rec["recipe_name"]
+    description = rec["recipe_description"]
+    prep_time = rec["preparation_time"]
+    cook_time = rec["cooking_time"]
+    skill = rec["skill"]
+    portions = rec["portions"]
+
+    """
+    Below is the Ingredients and Steps which were
+    predeterment in the file lists.py
+    """
+    ingredient_list = recipe_ingredients(rec)
+    steps_list = recipe_steps(rec)
+
+    recipe.update_one({'_id': ObjectId(recipe_id)},
+                      {
+        "$set": {
             "recipe_name": name,
             "recipe_description": description,
             "recipe_prep_time": int(prep_time),
@@ -68,11 +112,11 @@ def insert_recipe():
             "recipe_skill": skill,
             "recipe_portions": int(portions),
             "recipe_steps": steps_list,
-            "recipe_ingredients": ingredient_list,
+            "recipe_ingredients": ingredient_list
         }
+    }
+    )
 
-    recipe = mongo.db.recipes
-    recipe.insert_one(new_recipe)
     return redirect(url_for('get_recipes'))
 
 
